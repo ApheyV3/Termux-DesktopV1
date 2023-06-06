0e484ec6f1e74188b2b21503fdbe78e73bc01e4b
ApheyV3 <130621925+ApheyV3@users.noreply.github.com>
2023-06-07 01:35:32 +0700

    Add files via upload

---
+++ termuxdesktop.sh
@@ -0,0 +1,10 @@
+termux-setup-storage
+apt install neofetch
+neofetch
+apt install x11-repo
+apt install git
+apt install nano
+apt install curl
+apt install git
+apt install xfce4 tigervnc-standalone-server
+vncserver
